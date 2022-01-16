eeb7ea4e6bf0ffe7b3315e8993dd2b4a41347cfe
acronisv <acronisv@gmail.com>
2022-01-16 04:36:13 +0300

    Удаляет renderEntireTree и subscribe из index.js

--- src/index.js
+++ src/index.js
@@ -8,23 +8,23 @@ import store from './redux/redux-store'
 import { Provider } from 'react-redux';
 
 
-let renderEntireTree = (state) => {
+
   ReactDOM.render(
     <React.StrictMode>
       <Provider store={store}>
-        <App state = {state} dispatch={store.dispatch.bind(store)} store={store}/>
+        <App/>
       </Provider>
     </React.StrictMode>,
     document.getElementById('root')
   );
-}
 
-renderEntireTree(store.getState())
 
-store.subscribe(() => {
-  let state = store.getState()
-  renderEntireTree(state)
-})
+// renderEntireTree(store.getState())
+
+// store.subscribe(() => {
+//   let state = store.getState()
+//   renderEntireTree(state)
+// })
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
