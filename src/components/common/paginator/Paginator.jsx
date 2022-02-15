5540af0c057fe6152a4289e8aff4ed1467aa307e
acronisv <acronisv@gmail.com>
2022-02-16 01:00:52 +0300

    Реализует пагинацию списка пользователей #93

--- src/components/common/paginator/Paginator.jsx
+++ src/components/common/paginator/Paginator.jsx
@@ -1,24 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import usersStyle from './Paginator.module.css'
 
 let Paginator = (props) => {
     let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize)
         let pages = []
-
+        let partSize = 10
         for (let i=1; i <= pagesCount; i++) {
             pages.push(i)
-            if( i>50) {
-                break
-            }
         }
+
+        let partCount = Math.ceil(pagesCount/partSize)
+        let [partNumber, setPartNumber] = useState(1)
+        let leftPartCount =  (partNumber-1) * partSize + 1
+        let rightPartCount = partNumber * partSize
+        console.log(partCount, leftPartCount, rightPartCount)
     return (
         
         <div>
             <div>
-                {pages.map(p=>{
+                {partNumber > 1 &&
+                <button onClick={()=>{ setPartNumber(partNumber - 1)}}>Prev</button>}
+                {pages
+                    .filter(p => p >= leftPartCount && p <= rightPartCount)
+                    .map(p=>{
                     return <span onClick={(e)=>{props.onPageChanged(p)}}className={props.currentPage === p ? usersStyle.selectedPage : null}>{p} </span>
                 })}
-
+                {partCount > partNumber &&
+                <button onClick={()=>{ setPartNumber(partNumber + 1)}}>Next</button>}
             </div>
         </div>
     )
